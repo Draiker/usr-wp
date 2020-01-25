e695d455a09e51a94d11e571aa58aad7c5a5d474
Sergii Gavryliuk <draiker.ds@gmail.com>
2020-01-25 20:43:38 +0200

    Update plugin seriously-simple-stats to v1.2.6

--- wp-content/plugins/seriously-simple-stats/seriously-simple-stats.php
+++ wp-content/plugins/seriously-simple-stats/seriously-simple-stats.php
@@ -1,13 +1,13 @@
 <?php
 /*
  * Plugin Name: Seriously Simple Stats
- * Version: 1.2.3
+ * Version: 1.2.2
  * Plugin URI: https://wordpress.org/plugins/seriously-simple-stats
  * Description: Integrated analytics and stats tracking for Seriously Simple Podcasting.
  * Author: Castos
  * Author URI: https://www.castos.com/
  * Requires at least: 4.4
- * Tested up to: 5.1.1
+ * Tested up to: 4.8.2
  *
  * Text Domain: seriously-simple-stats
  * Domain Path: /languages
@@ -25,29 +25,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-use SeriouslySimpleStats\Classes\Stats;
-
-require_once 'vendor/autoload.php';
-
-define( 'SSP_STATS_VERSION', '1.2.3' );
-define( 'SSP_STATS_DIR_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+define( 'SSP_STATS_VERSION', '1.2.2' );
 
 if ( ! function_exists( 'is_ssp_active' ) ) {
-	require_once 'ssp-includes/ssp-functions.php';
+	require_once( 'ssp-includes/ssp-functions.php' );
 }
 
 if ( is_ssp_active( '1.13.1' ) ) {
 
+	// Load plugin class files
+	require_once( 'includes/class-ssp-stats.php' );
+
 	/**
 	 * Returns the main instance of SSP_Stats to prevent the need to use globals.
 	 *
-	 * @return Object Stats
 	 * @since  1.0.0
+	 * @return object SSP_Stats
 	 */
 	function SSP_Stats() {
-		$ssp_stats = Stats::instance( __FILE__, SSP_STATS_VERSION, '1.0.0' );
+		$instance = SSP_Stats::instance( __FILE__, SSP_STATS_VERSION, '1.0.0' );
 
-		return $ssp_stats;
+		return $instance;
 	}
 
 	SSP_Stats();
